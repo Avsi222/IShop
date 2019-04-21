8dea4296cf1d3ccf41758fc6568277de50b7d50f
«Avsi222» <dorogin.arseniy@yandex.ru>
2019-04-21 19:51:34 +0300

    init

---
+++ IShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IShop
+//
+//  Created by Арсений Дорогин on 21/04/2019.
+//  Copyright © 2019 Арсений Дорогин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
